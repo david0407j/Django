adb87c986300be748b1ec64c3c288f765c0085a2
Davidson <deivisonj1@hotmail.com>
2024-06-17 21:19:30 -0300

    html

--- projeto/base/tests/tests_home.py
+++ projeto/base/tests/tests_home.py
@@ -1,11 +1,19 @@
 from django.test import  Client
+from django.urls import reverse
+import pytest
 
 from projeto.django_assertions import assert_contains
 
-def test_status_code(client:Client):
-    resp = client.get(resp)
+@pytest.fixture
+def resp(client,):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+def test_status_code(resp):
     assert resp.status_code == 200 
 
 def test_title(resp):
-   assert_contains(resp, '<title>Reizinho-imports- home</title>' )
+   assert_contains(resp, '<title> Python Pro - home</title>' )
 
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
